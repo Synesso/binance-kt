98cdf6b7d417c6354496d3494707563ad9625929
Jem Mawson <jmawson@squareup.com>
2019-08-12 22:23:51 +1000

    Order book is updated dynamically by merging with updates

--- src/main/kotlin/binance/App.kt
+++ src/main/kotlin/binance/App.kt
@@ -9,11 +9,11 @@ fun main(args: Array<String>) {
     val factory = BinanceApiClientFactory.newInstance(args[0], args[1])
     val restClient = factory.newRestClient()
 
-    val xlmBtcOrderBook = restClient.getOrderBook("NEOBTC", 20)
-    println(xlmBtcOrderBook)
-    println(xlmBtcOrderBook.spread())
-    println(xlmBtcOrderBook.bestAsk())
-    val bestBid = xlmBtcOrderBook.bestBid()
+    val orderBook = restClient.getOrderBook("ETHBTC", 20)
+    println(orderBook)
+    println(orderBook.spread())
+    println(orderBook.bestAsk())
+    val bestBid = orderBook.bestBid()
     println(bestBid)
 //    println("Pegged: ${xlmBtcOrderBook.pegBidPrice("0.0000003", "0.00094500")}")
 
@@ -32,10 +32,10 @@ fun main(args: Array<String>) {
 
     val wsClient = factory.newWebSocketClient()
 
-    wsClient.onDepthEvent("ethbtc", { response: DepthEvent ->
-        println(response)
-//        println(restClient.getOrderBook("ethbtc", 5).asks)
-//        println()
+    wsClient.onDepthEvent("ethbtc", { event: DepthEvent ->
+        println("adding ${event.asks}")
+        orderBook.update(event)
+        println("orderbook: ${orderBook.asks}")
     })
 
 }
